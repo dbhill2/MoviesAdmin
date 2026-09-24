e76fd8bdd486f72a163c236430a6b67ec54e96f7
dbhill2 <w0440420@nscc.ca>
2026-09-24 13:25:21 -0300

    Added login credential model for index

--- MoviesAdmin/Controllers/HomeController.cs
+++ MoviesAdmin/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace MoviesAdmin.Controllers
         public IActionResult Index()
         {
 
-            return View();
+            return View(new LoginCred());
         }
 
         public IActionResult Privacy()
---
+++ MoviesAdmin/Models/LoginCred.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class LoginCred
+    {
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
